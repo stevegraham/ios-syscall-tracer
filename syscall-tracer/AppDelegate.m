2c5be78f2b410129767fd8d3b2a4a43b5f887c39
Stevie Graham <sg@teller.io>
2018-05-15 13:36:05 +0100

    Initial commit

---
+++ syscall-tracer/AppDelegate.m
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.m
+//  syscall-tracer
+//
+//  Created by Stevie Graham on 13/05/2018.
+//  Copyright © 2018 Teller. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    
+    NSLog(@"PT_DENY_ATTACH");
+    
+    /* Calling ptrace with PT_DENY_ATTACH with inline assembly, because:
+    /  - Want to prove the capability of interposing syscalls from inline ASM.
+    /  - PT_DENY_ATTACH will immediately crash the app if launched by Xcode/lldb.
+    */
+    
+    __asm__ __volatile__("mov x0, #31");
+    __asm__ __volatile__("mov x1, #0");
+    __asm__ __volatile__("mov x2, #0");
+    __asm__ __volatile__("mov x3, #0");
+    __asm__ __volatile__("mov x16, #26");
+    __asm__ __volatile__("svc #128");
+
+    return YES;
+}
+@end
---
+++ syscall-tracer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  syscall-tracer
+//
+//  Created by Stevie Graham on 13/05/2018.
+//  Copyright © 2018 Teller. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
